3ed25d48e5da548bf5de533e23757e42ec585113
Jony <jonathanblankleder95@gmail.com>
2025-09-09 15:03:26 -0300

    Add addition feature

---
+++ calculator.py
@@ -0,0 +1,16 @@
+class Calculator:
+"""
+Una clase simple de calculadora que implementa operaciones básicas.
+"""
+def add(self, a, b):
+    """
+    Suma dos números y retorna el resultado.
+
+    Args:
+        a (float): Primer número
+        b (float): Segundo número
+
+    Returns:
+        float: La suma de a y b
+    """
+    return a + b
